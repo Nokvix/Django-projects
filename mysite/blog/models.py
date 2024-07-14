1d1fbe19ee4ef5a20b087937ef20dcc08059aedd
Nokvix <igor.gass2003@mail.ru>
2024-07-14 17:01:54 +0500

    Переделал код на использования канонических URL-адресов для отображения деталей поста

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 
 # Create your models here.
@@ -17,7 +18,8 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
@@ -34,7 +36,14 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-publish']
-        # indexes = [models.Index(fields=['-publish'])]
+        indexes = [models.Index(fields=['-publish'])]
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
