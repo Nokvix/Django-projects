6ed8b7739fc24709b2eb85fed9b31d11629c8866
Nokvix <igor.gass2003@mail.ru>
2024-07-15 19:59:25 +0500

    Переделал представление списка постов на класс. Создал форму для отправки писем по электронной почте

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,20 +1,39 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .froms import EmailPostForm
 
 
 # Create your views here.
 
 
-def post_list(request):
-    post_published_list = Post.published.all()
+# def post_list(request):
+#     post_published_list = Post.published.all()
+#
+#     # Добавление пагинации по 3 поста на страницу
+#     paginator = Paginator(post_published_list, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#
+#     return render(request, 'blog/post/list.html', {'posts': posts})
 
-    # Добавление пагинации по 3 поста на страницу
-    paginator = Paginator(post_published_list, 3)
-    page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
 
-    return render(request, 'blog/post/list.html', {'posts': posts})
+class PostListView(ListView):
+    """
+    Альтернативный способ представления списка постов.
+    Аналог post_list
+    """
+
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
@@ -26,3 +45,24 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
 
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    # Извлекаю пост по id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+
+    if request.method == 'POST':
+        # Форма передана на обработку
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+
+    return render(request,
+                  'blog/post/share.html',
+                  {
+                      'post': post,
+                      'form': form
+                  })
