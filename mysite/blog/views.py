f4e5d7b63c67885bb622e3067c6c07f08889d29d
Nokvix <igor.gass2003@mail.ru>
2024-07-20 22:36:13 +0500

    Добавил функционал извлечения постов по сходству и создал несколько конкретно-прикладных шаблонных тегов и фильтров

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,6 +6,7 @@ from django.core.mail import send_mail, EmailMessage
 from .forms import EmailPostForm, CommentForm
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 
 # Create your views here.
@@ -61,12 +62,17 @@ def post_detail(request, year: int, month: int, day: int, post: str):
     # Форма для комментирования
     form = CommentForm()
 
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {
                       'post': post,
                       'comments': comments,
                       'form': form,
+                      'similar_posts': similar_posts,
                   })
 
 
