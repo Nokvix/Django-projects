878a645611fcafe6aa9b2c1f6c689d0e8c9d72c5
Nokvix <igor.gass2003@mail.ru>
2024-09-22 20:03:42 +0500

    Добавил функционал регистрации пользователя

--- social_website/bookmarks/account/forms.py
+++ social_website/bookmarks/account/forms.py
@@ -1,6 +1,27 @@
+from django.contrib.auth.models import User
 from django import forms
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email']
+
+    username = forms.CharField(label='Никнейм')
+    first_name = forms.CharField(label='Имя')
+    email = forms.EmailField(label='Почта')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Пароли не совпадают')
+
+        return cd['password2']
